6f8fcec165c02143d70199efdd4ef6112edf4fde
KustovYuri <iurban.kustov.200@gmail.com>
2025-12-07 15:59:41 +0300

    Remove backend to re-add as subtree

--- backend/src/main/kotlin/com/parkinsoft/backend/models/model/TestResultModel.kt
+++
@@ -1,8 +0,0 @@
-package com.parkinsoft.backend.models.model
-
-data class TestResultModel(
-    val testQuestion: String,
-    val testAnswer: String,
-    val testScore: Int,
-    val testMaxScope: Int
-)
